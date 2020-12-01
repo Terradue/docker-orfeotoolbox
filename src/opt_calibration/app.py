6311c338856775beb11f71274c04566391edbfe2
fabrice.brito@terradue.com <fabrice.brito@terradue.com>
2020-12-01 18:03:07 +0100

    added STAC output

--- src/opt_calibration/app.py
+++ src/opt_calibration/app.py
@@ -2,7 +2,10 @@ import os
 import sys
 import logging
 import click
-from .optcal import otb_opt_calibration
+import shutil
+from pystac import Item, Catalog, CatalogType, extensions
+
+from .optcal import otb_opt_calibration, get_bands, get_asset
 from .stac import get_item
 
 logging.basicConfig(stream=sys.stderr, 
@@ -21,8 +24,64 @@ def main(input_reference):
 
     item = get_item(os.path.join(input_reference, 'catalog.json')) 
     
-    otb_opt_calibration(item, 'red')
+    os.mkdir(item.id)
     
+    bands = get_bands(item)
+    
+    #cal_assets = []
+    
+    out_item = Item(id=item.id,
+                   geometry=item.geometry,
+                   bbox=item.bbox,
+                   datetime=item.datetime,
+                   properties=item.properties)
+    for band in bands: 
+        
+        asset, asset_href = get_asset(item, band)
+        
+        if band == 'pan':
+                        
+            shutil.copy(asset_href, os.path.join(item.id, 'pan.tif'))
+            
+            asset.href = 'pan.tif'
+            
+            out_item.add_asset(band, asset)
+            
+        else:
+            
+            cal_tif = otb_opt_calibration(item, band)
+            
+            shutil.move(cal_tif, os.path.join(item.id, cal_tif))
+    
+            asset.href = cal_tif
+            dir(asset)
+            out_item.add_asset(band, asset)
+            
+            #cal_assets.append(asset)
+      
+    eo_item = extensions.eo.EOItemExt(item)
+    
+    eo_bands = []
+
+    for band in eo_item.bands: 
+
+        band.name = band.common_name
+
+        eo_bands.append(band)
+        
+    eo_item.apply(eo_bands)
+    
+    # to STAC
+    logging.info('STAC')
+    cat = Catalog(id=item.id,
+                  description="Calibrated optical product") 
+
+    cat.add_items([out_item])
+    
+    cat.normalize_and_save(root_href='./',
+                           catalog_type=CatalogType.SELF_CONTAINED)
+     
+    logging.info('Done!')
     
 if __name__ == '__main__':
     main()
--- src/opt_calibration/optcal.py
+++ src/opt_calibration/optcal.py
@@ -1,6 +1,17 @@
+import os
 from urllib.parse import urlparse
 from pystac import *
 import otbApplication
+import gdal
+import logging
+import sys 
+import numpy as np
+from time import sleep
+
+logging.basicConfig(stream=sys.stderr, 
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%dT%H:%M:%S')
 
 def fix_asset_href(uri):
     #print('-', uri)
@@ -14,6 +25,19 @@ def fix_asset_href(uri):
         
         return uri
 
+    
+def get_bands(item):
+    
+    eo_item = extensions.eo.EOItemExt(item)
+    
+    bands = None
+    
+    if (eo_item.bands) is not None:
+    
+        bands = [band.common_name for band in eo_item.bands]
+    
+    return bands
+    
 def get_asset(item, band_name):
     
     asset = None
@@ -109,13 +133,68 @@ def otb_opt_calibration(item, common_band_name, level='toa'):
 
     app.SetParameterString('atmo.aerosol', 'noaersol')
 
-    app.SetParameterString('out', '{}.tif'.format(common_band_name))
+    app.SetParameterString('out', 'f_{}.tif'.format(common_band_name))
 
     app.SetParameterString("milli", '0')
-    app.SetParameterString("clamp", '0')
+    app.SetParameterString("clamp", '1')
 
     #app.SetParameterOutputImagePixelType("out", otbApplication.ImagePixelType_int16)
     
     app.ExecuteAndWriteOutput()
 
+    rescale('f_{}.tif'.format(common_band_name), 
+            '{}.tif'.format(common_band_name))
+    
+    os.remove('f_{}.tif'.format(common_band_name))
+    
     return '{}.tif'.format(common_band_name)
+
+def rescale(in_tif, out_tif):
+    
+    scaling_factor = 10000
+    
+    ds = gdal.Open(in_tif)
+    
+    width = ds.RasterXSize
+    height = ds.RasterYSize
+
+    input_geotransform = ds.GetGeoTransform()
+    input_georef = ds.GetProjectionRef()
+    
+    in_band = ds.GetRasterBand(1)
+    
+    in_arr = in_band.ReadAsArray()
+    
+    driver = gdal.GetDriverByName('GTiff')
+
+    output = driver.Create(out_tif, 
+                           width, 
+                           height, 
+                           1, 
+                           gdal.GDT_Int16)
+
+    output.SetGeoTransform(input_geotransform)
+    output.SetProjection(input_georef)
+
+    logging.info('Converting band to Int16')
+
+    band = output.GetRasterBand(1)
+
+    band.WriteArray((in_arr * scaling_factor).astype(np.intc))
+
+    output.FlushCache()
+
+    band = None
+
+    output = None
+
+    sleep(5)
+    
+    del(ds)
+    del(output)
+    
+    in_band = None
+    ds = None
+    del(ds)
+    
+    return True
